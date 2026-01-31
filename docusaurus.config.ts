73ad57db1aa872869971921793d2ccb2483fbba5
DuckLL <duckll@users.noreply.github.com>
2026-01-31 22:40:20 +0800

    pikaball

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -95,7 +95,7 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/logo.jpg",
     colorMode: {
-      defaultMode: "dark",
+      defaultMode: "light",
       respectPrefersColorScheme: false,
     },
     navbar: {
